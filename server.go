56ee5d646a831a6483d1ba2c43141776955e113d
Tomas Lingotti <tomaslingotti@gmail.com>
2023-07-08 17:22:17 -0300

    include zerologger from CHI

--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httplog"
-	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -15,45 +15,52 @@ import (
 
 type Server struct {
 	s *http.Server
+
+	logger zerolog.Logger
+
 	*chi.Mux
 }
 
 // NewServer return a *Server.
 func NewServer(port, serviceName string) *Server {
-	srv := newServer(port)
+	srv := newServer(port, serviceName)
+
+	srv.Use(
+		httplog.RequestLogger(srv.logger),
+		middleware.Heartbeat("/ping"),
+	)
+	return srv
+}
+
+func newServer(port, serviceName string) *Server {
+	mux := chi.NewMux()
+
 	logger := httplog.NewLogger(serviceName, httplog.Options{
 		JSON:            true,
 		Concise:         true,
 		TimeFieldFormat: time.UnixDate,
 	})
 
-	srv.Use(
-		httplog.RequestLogger(logger),
-		middleware.Heartbeat("/ping"),
-	)
-	return srv
-}
-
-func newServer(port string) *Server {
-	mux := chi.NewMux()
 	return &Server{
-		Mux: mux,
+		Mux:    mux,
+		logger: logger,
 		s: &http.Server{
 			Addr:         ":" + port,
 			WriteTimeout: time.Second * 15,
 			ReadTimeout:  time.Second * 15,
 			IdleTimeout:  time.Second * 60,
-			Handler:      mux,
+
+			Handler: mux,
 		},
 	}
 }
 
 // Start runs ListenAndServe on the http.Server with graceful shutdown.
 func (s *Server) Start() {
-	log.Info().Msgf("server is running on port %s", s.s.Addr)
+	s.logger.Info().Msgf("server is running on port %s", s.s.Addr)
 	go func() {
 		if err := s.s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Error().Msgf("closed Server error %s", err.Error())
+			s.logger.Error().Msgf("closed Server error %s", err.Error())
 		}
 	}()
 	s.gracefulShutdown()
@@ -64,16 +71,16 @@ func (s *Server) gracefulShutdown() {
 
 	signal.Notify(quit, syscall.SIGINT)
 	sig := <-quit
-	log.Info().Msgf("server is shutting down %s", sig.String())
+	s.logger.Info().Msgf("server is shutting down %s", sig.String())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	s.s.SetKeepAlivesEnabled(false)
 	if err := s.s.Shutdown(ctx); err != nil {
-		log.Error().Msgf("could not gracefully shutdown the Server %s", err.Error())
+		s.logger.Error().Msgf("could not gracefully shutdown the Server %s", err.Error())
 	}
-	log.Info().Msg("server stopped")
+	s.logger.Info().Msg("server stopped")
 }
 
 type WebHandler func(w http.ResponseWriter, r *http.Request) error
--- web.go
+++ web.go
@@ -8,10 +8,10 @@ import (
 )
 
 type HttpResponse struct {
-	Message   string      `json:"message"`
-	Data      interface{} `json:"data,omitempty"`
-	Success   bool        `json:"success"`
-	RequestID string      `json:"request_id,omitempty"`
+	Message   string `json:"message"`
+	Data      any    `json:"data,omitempty"`
+	Success   bool   `json:"success"`
+	RequestID string `json:"request_id,omitempty"`
 }
 
 type ErrHTTP struct {
@@ -55,8 +55,8 @@ func EJson(w io.Writer, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
-// Djson is a generic way to unmarshal your JSON.
-func Djson[t any](b io.ReadCloser, target *t) (*t, error) {
+// DJson is a generic way to unmarshal your JSON.
+func DJson[t any](b io.ReadCloser, target *t) (*t, error) {
 	defer func() {
 		_ = b.Close()
 	}()
@@ -67,4 +67,45 @@ func Djson[t any](b io.ReadCloser, target *t) (*t, error) {
 		return nil, err
 	}
 	return target, nil
+}
+
+// Response is a wrapper for web handlers that are compliant with the stdlib.
+func Response(status int, w http.ResponseWriter, msg string, data any) {
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(status)
+
+	_ = json.NewEncoder(w).Encode(HttpResponse{
+		Message: msg,
+		Data:    data,
+		Success: status < 399,
+	})
+}
+
+// Res is a wrapper for web handlers that are compliant with the stdlib signature but returning an error.
+func Res(status int, w http.ResponseWriter, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+// ResErr is a wrapper for web handlers that are compliant with the stdlib signature but returning an error.
+func ResErr(status int, w http.ResponseWriter, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(data)
+}
+
+// ResponseErr is a wrapper for web handlers that are compliant with the stdlib signature but returning an error.
+func ResponseErr(status int, w http.ResponseWriter, msg string, data any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(HttpResponse{
+		Message: msg,
+		Data:    data,
+		Success: status < 399,
+	})
 }
